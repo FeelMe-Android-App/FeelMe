c9ae8c0e12a4de5997c71839cb5bb86f7ebdd8d1
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-08-04 10:57:17 -0300

    app created

---
+++ app/src/main/java/com/feelme/feelmeapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.feelme.feelmeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feelme.feelmeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
