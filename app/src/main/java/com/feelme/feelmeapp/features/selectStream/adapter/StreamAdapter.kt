accb13105ced9b498932fb7c62e02602d499d922
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-08-23 16:48:29 -0300

    fix: added parameter for streaming selection

--- app/src/main/java/com/feelme/feelmeapp/features/selectStream/adapter/StreamAdapter.kt
+++ app/src/main/java/com/feelme/feelmeapp/features/selectStream/adapter/StreamAdapter.kt
@@ -1,22 +1,26 @@
 package com.feelme.feelmeapp.features.selectStream.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.feelme.feelmeapp.R
 import com.feelme.feelmeapp.databinding.StreamItemBinding
 import com.feelme.feelmeapp.features.selectStream.model.StreamItem
+import kotlin.coroutines.coroutineContext
 
 class StreamAdapter(
     private val streamings: List<StreamItem>,
     private val onClickListener: (streaming: StreamItem) -> Unit
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+): RecyclerView.Adapter<StreamAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StreamAdapter.ViewHolder {
         val binding = StreamItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(streamings[position], onClickListener)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(streamings[position], onClickListener)
     }
 
     override fun getItemCount() = streamings.count()
@@ -26,12 +30,21 @@ class StreamAdapter(
             streaming: StreamItem,
             onClickListener: (streaming: StreamItem) -> Unit
         ) {
+            if(streaming.selected) binding.ivStreamerLogo.alpha = 1F
+            else binding.ivStreamerLogo.alpha = 0.3F
+
             binding.ivStreamerLogo.setImageResource(streaming.image)
             binding.ivStreamerLogo.setOnClickListener {
-                if(binding.ivStreamerLogo.alpha != 1F) binding.ivStreamerLogo.alpha = 1F
-                else binding.ivStreamerLogo.alpha = 0.3F
+                streaming.selected = !streaming.selected
+                if(streaming.selected) {
+                    binding.ivStreamerLogo.alpha = 1F
+                } else {
+                    binding.ivStreamerLogo.alpha = 0.3F
+                }
                 onClickListener(streaming)
             }
         }
     }
+
+
 }
--- app/src/main/java/com/feelme/feelmeapp/features/selectStream/model/StreamItem.kt
+++ app/src/main/java/com/feelme/feelmeapp/features/selectStream/model/StreamItem.kt
@@ -3,5 +3,6 @@ package com.feelme.feelmeapp.features.selectStream.model
 data class StreamItem (
     val id: Int,
     val image: Int,
-    val name: String
+    val name: String,
+    var selected: Boolean = false,
 )
